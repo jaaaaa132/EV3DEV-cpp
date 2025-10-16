778da89471c3b286ce4ea4d6149f69211948b130
jaaaaa132 <martin.slavkovsky09@gmail.com>
2025-10-16 18:34:19 +0200

    added file sending
    translation not finished

--- costume_programing_language/demo/send_file_to_robot.gd
+++ costume_programing_language/demo/send_file_to_robot.gd
@@ -1,16 +1,22 @@
 extends Button
 
 var file_path: String
+var ip :String
 
 func _on_pressed() -> void:
 	var panel: Panel = get_node("Panel")
 	panel.visible = !panel.visible
 
 func _on_file_button_pressed() -> void:
-	$Panel/Options/VBoxContainer/File/Load.visible = true
+	get_node("Panel/VBoxContainer/File/Load").visible = true
 
 func _on_file_selected(path: String) -> void:
 	file_path = path
 
 func _on_send_pressed() -> void:
-	pass # finish next time
+	ip = $Panel/VBoxContainer/HBoxContainer2/IP.text
+	var args = [
+		"-p", "maker",
+		"scp", file_path, "robot@" + ip + ":/home/robot/libc++/"
+	]
+	OS.execute("sshpass", args)
