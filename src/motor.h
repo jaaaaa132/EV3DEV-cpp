a0b04dd860e86a08614c674ffd92c6109aa703c8
jaaaaa132 <134095580+jaaaaa132@users.noreply.github.com>
2026-02-24 16:46:17 +0100

    Change default stop_action to 'brake' for motor methods

--- src/motor.h
+++ src/motor.h
@@ -38,12 +38,12 @@ public:
 	bool is_connected();
   void set_directory(std::string p_directory);
 	void run(int speed, int acceleration);
-  void run_to_abs_pos(int position, int speed, const char* stop_action = "hold");
-  void run_to_rel_pos(int position, int speed, const char* stop_action = "hold");
-  void run_for_time(int time_ms, int speed, const char* stop_action = "hold");
-  void stop(const char* stop_action = "hold");
+  void run_to_abs_pos(int position, int speed, const char* stop_action = "brake");
+  void run_to_rel_pos(int position, int speed, const char* stop_action = "brake");
+  void run_for_time(int time_ms, int speed, const char* stop_action = "brake");
+  void stop(const char* stop_action = "brake");
   void run_direct(int duty_cycle, bool inverted = false);
-  void run_direct_for_time(int duty_cycle, int time_ms, bool inverted = false, const char* stop_action = "hold");
+  void run_direct_for_time(int duty_cycle, int time_ms, bool inverted = false, const char* stop_action = "brake");
   std::string get_state();
   int get_position();
   void set_position(int new_position);
