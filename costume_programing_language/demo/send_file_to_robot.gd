2ac2ba4dff5cdcac4d63cf7e17f2cf36fe155db9
jaaaaa132 <martin.slavkovsky09@gmail.com>
2026-03-08 00:41:40 +0100

    send file supports windows
    not tested yet

--- costume_programing_language/demo/send_file_to_robot.gd
+++ costume_programing_language/demo/send_file_to_robot.gd
@@ -14,9 +14,32 @@ func _on_file_selected(path: String) -> void:
 	file_path = path
 
 func _on_send_pressed() -> void:
+	send_file()
+
+func send_file():
+	var os_name = OS.get_name()
+	var user = "robot"
+	var password = "maker"
 	ip = $Panel/VBoxContainer/HBoxContainer2/IP.text
-	var args = [
-		"-p", "maker",
-		"scp", file_path, "robot@" + ip + ":/home/robot/libc++/"
-	]
-	OS.execute("sshpass", args)
+	var target = "%s@%s:/home/robot/libc++/" % [user, ip]
+
+	if os_name == "Windows":
+		print("windows")
+		# Use PuTTY's pscp.exe
+		var args = [
+			"-pw", password,
+			file_path,
+			target
+			]
+		OS.execute("pscp.exe", args)
+
+	elif os_name == "X11" or os_name == "Linux":
+		print("linux")
+		# Use sshpass + scp
+		var args = [
+			"-p", password,
+			"scp", file_path, target
+			]
+		OS.execute("sshpass", args)
+	else:
+		print("unsupported OS")
