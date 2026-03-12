2e995bc5ef2673f9adba264f2e0cb9e44e800bf9
jaaaaa132 <134095580+jaaaaa132@users.noreply.github.com>
2026-03-12 23:07:21 +0100

    Update docker run commands to use current user

--- compile_all.sh
+++ compile_all.sh
@@ -1,14 +1,14 @@
 set -euo pipefail
 
-docker run --rm -v $(pwd):/src ev3dev-cpp-compiler -mcpu=arm926ej-s -march=armv5te -marm -O3 -Wl,--gc-sections -fdata-sections -ffunction-sections -Wall -Wextra -Wshadow -Werror=return-type -flto src/mains/main1.cpp src/motor.cpp src/position.cpp src/sensor.cpp src/diferential_drive.cpp src/robot.cpp -o main1 -std=c++14 -lm -pthread -lstdc++fs
+docker run --rm --user $(id -u):$(id -g) -v $(pwd):/src ev3dev-cpp-compiler -mcpu=arm926ej-s -march=armv5te -marm -O3 -Wl,--gc-sections -fdata-sections -ffunction-sections -Wall -Wextra -Wshadow -Werror=return-type -flto src/mains/main1.cpp src/motor.cpp src/position.cpp src/sensor.cpp src/diferential_drive.cpp src/robot.cpp -o main1 -std=c++14 -lm -pthread -lstdc++fs
 echo "main1 compiled"
-docker run --rm -v $(pwd):/src ev3dev-cpp-compiler -mcpu=arm926ej-s -march=armv5te -marm -O3 -Wl,--gc-sections -fdata-sections -ffunction-sections -Wall -Wextra -Wshadow -Werror=return-type -flto src/mains/main2.cpp src/motor.cpp src/position.cpp src/sensor.cpp src/diferential_drive.cpp src/robot.cpp -o main2 -std=c++14 -lm -pthread -lstdc++fs
+docker run --rm --user $(id -u):$(id -g) -v $(pwd):/src ev3dev-cpp-compiler -mcpu=arm926ej-s -march=armv5te -marm -O3 -Wl,--gc-sections -fdata-sections -ffunction-sections -Wall -Wextra -Wshadow -Werror=return-type -flto src/mains/main2.cpp src/motor.cpp src/position.cpp src/sensor.cpp src/diferential_drive.cpp src/robot.cpp -o main2 -std=c++14 -lm -pthread -lstdc++fs
 echo "main2 compiled"
-docker run --rm -v $(pwd):/src ev3dev-cpp-compiler -mcpu=arm926ej-s -march=armv5te -marm -O3 -Wl,--gc-sections -fdata-sections -ffunction-sections -Wall -Wextra -Wshadow -Werror=return-type -flto src/mains/main3.cpp src/motor.cpp src/position.cpp src/sensor.cpp src/diferential_drive.cpp src/robot.cpp -o main3 -std=c++14 -lm -pthread -lstdc++fs
+docker run --rm --user $(id -u):$(id -g) -v $(pwd):/src ev3dev-cpp-compiler -mcpu=arm926ej-s -march=armv5te -marm -O3 -Wl,--gc-sections -fdata-sections -ffunction-sections -Wall -Wextra -Wshadow -Werror=return-type -flto src/mains/main3.cpp src/motor.cpp src/position.cpp src/sensor.cpp src/diferential_drive.cpp src/robot.cpp -o main3 -std=c++14 -lm -pthread -lstdc++fs
 echo "main3 compiled"
-docker run --rm -v $(pwd):/src ev3dev-cpp-compiler -mcpu=arm926ej-s -march=armv5te -marm -O3 -Wl,--gc-sections -fdata-sections -ffunction-sections -Wall -Wextra -Wshadow -Werror=return-type -flto src/mains/main4.cpp src/motor.cpp src/position.cpp src/sensor.cpp src/diferential_drive.cpp src/robot.cpp -o main4 -std=c++14 -lm -pthread -lstdc++fs
+docker run --rm --user $(id -u):$(id -g) -v $(pwd):/src ev3dev-cpp-compiler -mcpu=arm926ej-s -march=armv5te -marm -O3 -Wl,--gc-sections -fdata-sections -ffunction-sections -Wall -Wextra -Wshadow -Werror=return-type -flto src/mains/main4.cpp src/motor.cpp src/position.cpp src/sensor.cpp src/diferential_drive.cpp src/robot.cpp -o main4 -std=c++14 -lm -pthread -lstdc++fs
 echo "main4 compiled"
-docker run --rm -v $(pwd):/src ev3dev-cpp-compiler -mcpu=arm926ej-s -march=armv5te -marm -O3 -Wl,--gc-sections -fdata-sections -ffunction-sections -Wall -Wextra -Wshadow -Werror=return-type -flto src/mains/main5.cpp src/motor.cpp src/position.cpp src/sensor.cpp src/diferential_drive.cpp src/robot.cpp -o main5 -std=c++14 -lm -pthread -lstdc++fs
+docker run --rm --user $(id -u):$(id -g) -v $(pwd):/src ev3dev-cpp-compiler -mcpu=arm926ej-s -march=armv5te -marm -O3 -Wl,--gc-sections -fdata-sections -ffunction-sections -Wall -Wextra -Wshadow -Werror=return-type -flto src/mains/main5.cpp src/motor.cpp src/position.cpp src/sensor.cpp src/diferential_drive.cpp src/robot.cpp -o main5 -std=c++14 -lm -pthread -lstdc++fs
 echo "main5 compiled"
 
 #scp main robot@10.42.0.3:/home/robot/libc++/
